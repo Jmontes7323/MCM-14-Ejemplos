0b792e8150c5849109664f674b7cd57fc2ec051b
Jordi <66032634+Jmontes7323@users.noreply.github.com>
2020-05-27 14:26:57 -0500

    Create recursion.js

---
+++ recursion.js
@@ -0,0 +1,9 @@
+función  factorial ( número )  {
+if  ( número  ==  1 )  {
+ número de retorno ;
+}
+más  {
+ número de retorno * factorial ( número - 1 ) ;
+}
+}
+consola . log ( factorial ( 5 ) ) ;
